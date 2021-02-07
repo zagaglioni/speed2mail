aa55709554d55eb32e1be743eedadc862c1a7ec7
zagaglioni <zagaglioni@gmail.com>
2021-02-07 22:07:48 +0100

    Update speed.sh

--- speed.sh
+++ speed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 now="$(date)"
-speedtest >> /usr/src/speed2mail/speedlog.txt
-(echo "Subject: $HOSTNAME - $now"; cat /usr/src/speed2mail/speedlog.txt; ) | sendmail zagaglioni@gmail.com
+speedtest >> $HOME/speed2mail/speedlog.txt
+(echo "Subject: $HOSTNAME - $now"; cat $HOME/speed2mail/speedlog.txt; ) | sendmail zagaglioni@gmail.com
